4c48535b5dfc68da296e6ddcb2682efb311d375c
tinayetsina <tinayetsina@outlook.com>
2024-12-28 11:25:02 +0100

    update - installed torch

--- model.py
+++ model.py
@@ -1,2 +1,11 @@
-print("Hello World")
+import torch
+import torch.nn
+
+class InputEmbeddings(nn.Module):
+    
+    def __init__(self, d_model: int, vocab_size: int):
+        super().__init__()
+        self.d_model = d_model
+        self.vocab_size = vocab_size
+
 
